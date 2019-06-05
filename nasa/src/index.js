9925af9601f2054bced23b0a6d6d0e7ffa7c90b8
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-06-05 13:23:12 -0500

    index.js set up

--- nasa/src/index.js
+++ nasa/src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
 
 
