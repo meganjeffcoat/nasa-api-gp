6cfffd77a5726c1141b9803141a15d6ec95efce7
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-06-05 13:26:38 -0500

    app imported in index.js, app.js set up and styles added into app.css

--- nasa/src/App.js
+++ nasa/src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
+
+import NASAPhoto from "./components/NASAPhoto";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NASAPhoto />
     </div>
   );
 }
--- nasa/src/index.js
+++ nasa/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
+import App from './App';
 import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
